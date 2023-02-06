7f9f7fd63adb82869d49e28930635fedbc1acd8c
Brian Takita <brian.takita@gmail.com>
2023-02-06 04:08:16 -0500

    - initial version: + FetchEvent_w_ctx_T,PageEvent_w_ctx_T,APIEvent_w_ctx_T: extracted from @ctx-core/ui-solid
    + .gitignore
    
    - node_modules,COMMIT_EDITMSG

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# @ctx-core/solid-start
+
+## 1.0.0
+
+### Major Changes
+
+- initial version: + FetchEvent_w_ctx_T,PageEvent_w_ctx_T,APIEvent_w_ctx_T: extracted from @ctx-core/ui-solid
---
+++ src/_types/index.d.ts
@@ -0,0 +1,15 @@
+import type { Ctx } from '@ctx-core/object'
+import type { APIEvent } from 'solid-start'
+import type { FetchEvent, PageEvent, ServerFunctionEvent } from 'solid-start/server/types'
+export type FetchEvent_w_ctx_T = (FetchEvent|ServerFunctionEvent|APIEvent|PageEvent)&{
+	locals:FetchEvent__locals_T
+}
+export type PageEvent_w_ctx_T = PageEvent&({
+	locals:FetchEvent__locals_T
+})
+export type APIEvent_w_ctx_T = FetchEvent&APIEvent&({
+	locals:FetchEvent__locals_T
+})
+type FetchEvent__locals_T = Record<string, unknown>&{
+	ctx:Ctx
+}
